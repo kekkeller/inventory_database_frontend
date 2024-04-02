7ba7436828e9f759fd9a04317eaaced84d17d4fa
Syxdev <dev@syxdev.de>
2024-04-02 14:57:27 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  ...
+}
