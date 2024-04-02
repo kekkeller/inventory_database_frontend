0e42fc688bd605b2b98ff19044d240a29a10c782
Syxdev <dev@syxdev.de>
2024-04-02 15:07:47 +0300

    change base project

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,32 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+// import NavBar from './components/NavBar.vue'
+import { defineComponent } from 'vue'
+import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+  components: { NavBar, Footer },
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/db.jpg" width="125px" height="125px" />
-    <div class="wrapper">
-      <HelloWorld msg="Your All in One Inventory Database" />
-    </div>
-  </header>
-
-  <main>
-   <TheWelcome />
-  </main>
+  <div class="flex flex-col h-screen justify-between">
+    <NavBar />
+    <router-view class="container max-w-8xl mx-auto mb-auto px-4" />
+    <Footer />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-.wrapper{
-  text-align: center;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  ...
 }
