e04ed2a87c948202af14907275a55e1c69d531cb
Syxdev <dev@syxdev.de>
2024-05-03 01:54:36 +0200

    make it sexy

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { createPinia } from 'pinia'
 import  BootstrapVue3  from 'bootstrap-vue-3';
 import { BootstrapIconsPlugin } from "bootstrap-icons-vue";
 
+
 import './css/index.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
